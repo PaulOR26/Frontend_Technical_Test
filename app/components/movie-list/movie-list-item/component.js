47b0581b451b68fd122d114991c5fad3d8263570
PaulOR26 <pj_orourke@outlook.com>
2021-09-11 14:21:55 +0100

    add functionality to edit the movie title

--- app/components/movie-list/movie-list-item/component.js
+++ app/components/movie-list/movie-list-item/component.js
@@ -1,8 +1,35 @@
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import { updateDoc } from 'firebase/firestore';
 import Component from '@glimmer/component';
 import podNames from 'ember-component-css/pod-names';
 
 export default class MovieListItem extends Component {
   styleNamespace = podNames['movie-list/movie-list-item'];
+
+  @tracked newTitle;
+
+  @tracked errorMessage;
+
+  @action async editMovie(event) {
+    event.preventDefault();
+
+    this.errorMessage = undefined;
+
+    try {
+      const { newTitle } = this;
+
+      await updateDoc(this.args.movie.ref, {
+        title: newTitle,
+      });
+
+      this.newTitle = undefined;
+
+      this.args.loadMovies();
+    } catch (error) {
+      this.errorMessage = error?.message;
+    }
+  }
 
   get movie() {
     return this.args.movie.data();

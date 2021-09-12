dce7b305fe14d28678ace86144fde395d136bfe9
PaulOR26 <pj_orourke@outlook.com>
2021-09-12 17:20:46 +0100

    add test for form title label text

---
+++ tests/integration/components/add-movie-form-test.js
@@ -0,0 +1,14 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | add-movie-form', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('title label has the correct text', async function (assert) {
+    await render(hbs`<AddMovieForm />`);
+
+    assert.dom('#add-movie-title-label').hasText('Title');
+  });
+});
